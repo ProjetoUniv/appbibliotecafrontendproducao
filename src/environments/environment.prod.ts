ff829cc87c6f701710ddebaeb8271cfa837e8fe5
aslac2020 <andredesenvolvedorti@gmail.com>
2021-10-31 16:42:00 -0300

    alterações

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://projeto-bibliotecaback.herokuapp.com/bibliotecainfantil/'
+  apiUrl: 'https://projeto-bibliotecaback.herokuapp.com/'
 };
